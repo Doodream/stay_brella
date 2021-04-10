2e269df1ad4a92c218f0ae42705f4fed41100837
Doodream <rhomo17@naver.com>
2021-04-11 01:26:55 +0900

    Add : src/pages/index.js : slideSection 적용

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Layout from '../components/Layout/Layout';
+import SlideSection from '../pages/home/section/SlideSection'
 import Banner from '../pages/home/section/Banner';
 
 
 export default function Home() {
     return (
         <Layout>
+            <SlideSection />
             <Banner />
         </Layout>
     )
