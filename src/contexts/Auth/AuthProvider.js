31207d4d40e792366a70dff38b248880c9833c05
Doodream <rhomo17@naver.com>
2021-04-10 18:07:58 +0900

    Add : src/contexts/Auth/AuthProvider.js : logout 기능 추가

---
+++ src/contexts/Auth/AuthProvider.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { withRouter, useRouter } from "next/router";
+import AuthContext from './AuthContext.js';
+import { Fetch } from '../../utils/Fetch';
+
+const AuthProvider = ({ children, localStorage }) => {
+
+    const [prevAuthUser, setPrevAuthUser] = React.useState(localStorage.user || {})
+    const router = useRouter();
+    const homeRedirect = () => router.push('/')
+    const saveUserInfo = res => {
+        if (!res.loginSuccess) {
+            throw new Error("아이디나 비밀번호가 맞지 않습니다.");
+        }
+        const newAuthUser = { ...prevAuthUser, ...res }
+        setPrevAuthUser(newAuthUser)
+        console.log(prevAuthUser);
+    }
+    const login = ({ email, password }) => Fetch.post('/api/login/', {
+        'email': email,
+        'password': password,
+    }).then(saveUserInfo).then(homeRedirect).catch(err => alert(err));
+
+    const logout = () => {
+        alert('로그아웃 되었습니다.')
+        setValue({ ...initialState, authUser: {}, isAuthenticated: false })
+        window.localStorage.clear()
+        router.push('/')
+    };
+
+
+
+    //state초기화 객체 입니다.
+    const initialState = {
+        saveUserInfo,
+        login,
+        kakaoLogin,
+        logout,
+        signUp,
+        kakaoSignUp,
+        uploadReview,
+        settingAccount,
+        authUser: prevAuthUser,
+        isAuthenticated: 'token' in prevAuthUser,
+
+    };
+    //Hook을 통한 state, setState를 정의합니다.
+    const [value, setValue] = React.useState(initialState);
+
+    React.useEffect(() => {
+        console.log('prevAuthUser change')
+        if (Object.keys(prevAuthUser).length > 0) {
+            console.log('prevAuthUser not null')
+            console.log(prevAuthUser)
+            setValue({
+                ...initialState,
+                authUser: prevAuthUser,
+                isAuthenticated: 'token' in prevAuthUser,
+            })
+        }
+    }, [prevAuthUser]);
+
+    React.useEffect(() => {
+        console.log('value change')
+        window.localStorage.clear()
+        window.localStorage['isAuthenticated'] = 'token' in value.authUser;
+        window.localStorage.setItem('user', JSON.stringify(value.authUser));
+    }, [value.authUser]);
+
+    return (
+        //AuthProvider에 state를 사용할 컴포넌트들을 호출하려면
+        //{children}이 있어야 합니다
+        //그래서 마지막 return에서 {children}을 리턴해줍니다.
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default withRouter(AuthProvider);
