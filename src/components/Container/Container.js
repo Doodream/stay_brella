6d7deeb7c6946d89188587899e73f7336971d5e5
Doodream <rhomo17@naver.com>
2021-04-13 02:03:07 +0900

    Add : src/components/Container/ : Container 컴포넌트 생성 : 페이지 타이틀

---
+++ src/components/Container/Container.js
@@ -0,0 +1,36 @@
+import React, { Children, useRef, useState } from 'react';
+import Link from 'next/link';
+
+import 'antd/dist/antd.css';
+import styled from 'styled-components';
+import { Button, message, Space, Divider, Input, Form, Checkbox } from 'antd';
+
+export default function Container({ title, children }) {
+
+    return (
+        <Outline>
+            <PageTitle>
+                <span>{title}</span>
+                <Divider style={{ boxShadow: '0 0 15px 0 rgb(2 59 109 / 10%)', margin: '10px 0 0 0' }} />
+            </PageTitle>
+            {children}
+        </Outline>
+    )
+
+}
+
+const Outline = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items:center;
+    justify-content: center;
+    background: white;
+`
+
+const PageTitle = styled.div`
+    width: 90%;
+    > span {
+        font-size: 2rem;
+        color: #34495e;
+    }
+`
