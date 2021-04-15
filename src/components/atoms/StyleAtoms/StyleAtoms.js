f10dbb84c675e41ff800beb41bfc494f563a1263
Doodream <rhomo17@naver.com>
2021-04-15 19:26:57 +0900

    Add : StyleAtoms.js : 자주 쓰는 스타일 컴포넌트화

---
+++ src/components/atoms/StyleAtoms/StyleAtoms.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+import styled from 'styled-components';
+
+const buttonStyle = {
+    background: '#34495e',
+    color: 'white'
+}
+
+const StyleButton = (props) => {
+    return (
+        <Button {...props} style={buttonStyle}>{props.children}</Button>
+    )
+}
+
+const StyleLabel = (props) => {
+    return (
+        <label {...props} style={{ fontSize: '20px' }}>{props.children}</label>
+    )
+}
+
+export { StyleButton, StyleLabel };
+
